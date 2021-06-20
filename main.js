d2eb8eacac02459eb65428c1dee1b88b404f01a0
miwazawa <61324099+miwazawa@users.noreply.github.com>
2021-06-20 16:47:44 +0900

    bug fix

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 function run(){
-    var out_str = "誘惑に弱いのね";
     var all_member = [];
     var checked_member = [];
     all_member = all_member.concat(Array.from(document.getElementsByName("member")));
     
     for (let i = 0; i< all_member.length; i++){
         if (all_member[i].checked){
-            checked_member.push(all_member[i].value + "/n");
+            checked_member.push(all_member[i].value + "\n");
         }
     }
 
-    var out_str = checked_member.join("") + "/nこんにちは";
-    document.getElementById("output").innerText = out_str;
+    if (checked_member.length == 0){
+        var out_str = "なんかチェックつけて";
+        document.getElementById("output").innerText = out_str;
+    }
+    else{
+        var out_str = checked_member.join("") + "こんにちは";
+        document.getElementById("output").innerText = out_str;
+    }
 }
